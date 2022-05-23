813093b21a94a4980b18bf0c7a51e0290d609241
CyberAriel <jastrzab.pp@gmail.com>
2022-05-23 20:37:59 +0200

    Create Project

---
+++ REST_API/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using REST_API.Models;
+using System.Net.Http;
+
+namespace REST_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ApiController
+    {
+
+        static readonly IProductRepository repository = new ProductRepository();
+
+        
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        
+        [HttpPost]
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        
+        [HttpPut("{id}")]
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void DeleteProduct(int id)
+        {
+            repository.Remove(id);
+        }
+    }
+}
+
---
+++ REST_API/Models/IProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace REST_API.Models
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add(Product item);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+}
+
